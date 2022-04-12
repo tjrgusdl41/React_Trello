4221e0be4aa74a465f9120bbac80b02e819aef6a
tjrgusdl41 <63176593+tjrgusdl41@users.noreply.github.com>
2022-04-12 21:00:20 +0900

    주석달기(중요함 매우중요함)

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,55 @@
-import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top:30px; 
-  background-color:${props => props.theme.boardColor};
-  border-radius:5px;
-  min-height: 200px;
-`;
-const Card = styled.div`
-margin-bottom:5px;
-border-radius:5px;
-padding : 10px 10px;
-background-color: ${props => props.theme.cardColor} ;
-`
-const Boards = styled.div` 
-  display:grid;
-  width:100%;
-  grid-template-columns: repeat(1,  1fr) ;
-`
+import Board from "./Components/Board";
 const Wrapper = styled.div`
+  width:100vw;
   display: flex;
-  max-width: 480px;
+  max-width: 680px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
   align-items: center;
   height: 100vh;
-`
+`;
+
+const Boards = styled.div`
+  justify-content: center;
+  align-items: flex-start;
+  display: flex;
+  width: 100%;
+  gap: 10px;
+`;
+
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-    if (!destination) return;
-    setToDos((oldTodos) => {
-      const toDosCopy = [...oldTodos];
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
-      toDosCopy.splice(source.index, 1);
-      console.log("Delete item");
-      console.log(toDosCopy);
-      console.log("Put back", draggableId, "on ", destination.index);
-      toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
-      return toDosCopy;
-    });
+  const onDragEnd = (info: DropResult ) => {
+    console.log(info); 
+    const { destination, draggableId, source } = info; 
+    if (destination?.droppableId === source.droppableId) {//도착지의 droppableId(BOX이름) 이랑 출발한droppableId 가 같으면 
+      //똑같은 박스에서 움직이는 로직관리
+      setToDos((allBoards) => { //투두에서 모든 박스들을 받아와서
+        const boardCopy = [...allBoards[destination?.droppableId]]; //전체보드 객체안의 현재보드를 전체복사한다
+        boardCopy.splice(source.index, 1);//시작점의 index에 1개요소 제거함.
+        boardCopy.splice(destination?.index, 0, draggableId);//도착점의 index에 0개요소를 draggableId(즉 이동시킨 카드로 바꿈)
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination?.droppableId !== source.droppableId) {
+      //cross board movement
+    }
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-        <Wrapper>
+      <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(magic) => (
-                      <Card
-                        ref={magic.innerRef}
-                        {...magic.dragHandleProps}
-                        {...magic.draggableProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
---
+++ src/Components/Board.tsx
@@ -0,0 +1,41 @@
+import { Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+import DraggableCard from "./DraggableCard";
+const Wrapper = styled.div`
+  width:300px;
+  padding-top:10px ;
+  padding: 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 300px;
+`;
+
+interface IBoardProps {
+  toDos: string[];
+  boardId: string;
+}
+
+const Title = styled.h2`
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+function Board({ toDos, boardId }: IBoardProps) {
+  return (
+    <Wrapper>
+      <Title>{boardId}</Title>
+    <Droppable droppableId={boardId}>
+      {(magic) => (
+        <div ref={magic.innerRef} {...magic.droppableProps}>
+          {toDos.map((toDo, index) => (
+            <DraggableCard key={toDo} index={index} toDo={toDo} />
+          ))}
+          {magic.placeholder}
+          </div>
+      )}
+      </Droppable>
+      </Wrapper>
+  );
+}
+export default Board;
